6d439c2edc4df0c11efd751f8647e89bfc4686c9
Sheng <dsheng1993@gmail.com>
2024-10-14 15:26:18 +0200

    fastpush

---
+++ data/BACKEND/trascendence/trascendence/views.py
@@ -0,0 +1,17 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreateAPIView(APIView):
+    def get(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
